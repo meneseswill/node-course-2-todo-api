8270874aa4039fc4b551872402d9d936477e2f40
Wilfredo <meneseswill@gmail.com>
2017-07-21 20:39:26 -0600

    Prueba

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds053788.mlab.com:53788/todoappmeneboni' ||'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://meneboni:Rojo2010.*@ds053788.mlab.com:53788/todoappmeneboni' ||'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
 	mongoose
