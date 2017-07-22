61a509943bfe5b264abded87062acb3990fe9262
Wilfredo <meneseswill@gmail.com>
2017-07-21 20:25:26 -0600

    Copia y pega nuevamente la ruta de la BD

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://meneboni:Rojo2010.*@ds053788.mlab.com:53788/todoappmeneboni' ||'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://meneboni:Rojo2010.*@ds053788.mlab.com:53788/todoappmeneboni');
 
 module.exports = {
 	mongoose
