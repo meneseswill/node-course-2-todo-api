3ecc8e53851d256df7c4eb65804d974cffd8f5db
Wilfredo <meneseswill@gmail.com>
2017-07-21 22:47:08 -0600

    Correcciones de conexión

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,8 @@ mongoose.Promise = global.Promise;
 var db = {
 	localhost:'mongodb://localhost:27017/TodoApp',
 	mlab: 'mongodb://meneboni:Rojo2010.*@ds053788.mlab.com:53788/todoappmeneboni'
-}
+};
+
 mongoose.connect(db.localhost || db.mlab);
 
 module.exports = {
